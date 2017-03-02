ea275d0a4f643b15675c0467ba235289489a7608
Chris Stanton <cstanton0760@yahoo.com>
2017-03-02 10:11:39 -0600

    corrected question 5 to lecture answer

--- assignment.sql
+++ assignment.sql
@@ -3,22 +3,24 @@
 1. Get all customers and their addresses
 
 SELECT *
-FROM customers, addresses;
+FROM customers
+JOIN addresses ON customers.id=addresses.customer_id;
 
 2. Get all orders and their line items
 
 SELECT *
-FROM orders, line_items;
+FROM line_items
+JOIN orders ON line_items.order_id=orders.id;
 
 3. Which warehouses have cheetos?
 
-SELECT warehouse
+SELECT warehouse.warehouse
 FROM warehouse
 JOIN warehouse_product
 ON warehouse.id = warehouse_product.warehouse_id
 JOIN products
 ON warehouse_product.product_id = products.id
-WHERE description = 'cheetos';
+WHERE products.description = 'cheetos';
 
 4. Which warehouses have diet pepsi?
 
@@ -34,27 +36,27 @@ GROUP BY warehouse;
 5. Get the number of orders for each customer.
 NOTE: It is OK if those without orders are not included in results.
 
-SELECT *
+SELECT customers.id, customers.first_name, customers.last_name, count(*)
 FROM orders
 JOIN addresses
-ON addresses.customer_id = orders.id
+ON orders.address_id=addresses.id
 JOIN customers
 ON customers.id = addresses.customer_id
-GROUP BY customers.id, orders.id, addresses.id;
+GROUP BY customers.id;
 
 6. How many customers do we have?
 
-SELECT COUNT(id)
+SELECT COUNT(*)
 FROM customers;
 
 7. How many products do we carry?
 
-SELECT COUNT(id)
+SELECT COUNT(*)
 FROM products;
 
 8. What is the total available on-hand quantity of diet pepsi?
 
-SELECT SUM(on_hand)
+SELECT SUM(warehouse_product.on_hand)
 FROM products
 JOIN warehouse_product
 ON products.id = warehouse_product.product_id
